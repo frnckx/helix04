3d7274df3cdb4c1c015b1b36e5f8fd6457bea42c
Hétfő <palotaszoltan55@gmail.com>
2017-12-06 18:34:55 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    UserRegistry userRegistry = new UserRegistry();
+    userRegistry.printMenu();
+    userRegistry.run();
+    }
+}
